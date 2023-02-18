0de36dba8d14c1834b9a3f0eb4d3281b00b7ad06
Tauhid <tauhid.tohibur@gmail.com>
2023-02-18 13:05:36 +0600

    Python All info file modified

--- All information.py
+++ All information.py
@@ -8,5 +8,10 @@ print(keyword.iskeyword("And"))   # Checks if And is a keyword
 
 
 def add(a, b):
+    a = 10
+    
+
     """Return the sum of given arguments."""
     return a + b
+
+print(add(10,200))
