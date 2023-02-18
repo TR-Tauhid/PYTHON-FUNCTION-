c1624d40fcf28698d5baa54a19e8e2a32dd5430d
Tauhid <tauhid.tohibur@gmail.com>
2023-02-18 13:20:16 +0600

    The new line of comment

--- All information.py
+++ All information.py
@@ -10,8 +10,9 @@ print(keyword.iskeyword("And"))   # Checks if And is a keyword
 def add(a, b):
     a = 10
     
-
+    """This is the new line """
     """Return the sum of given arguments."""
     return a + b
 
 print(add(10,200))
+print("THIS IS THE NEW LINE MODIFIED FOR GITHUB.")
