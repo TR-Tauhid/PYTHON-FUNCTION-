6fb156d1810794bd9ec38aec9b798c9ab9afdb25
Tauhid <tauhid.tohibur@gmail.com>
2023-02-18 15:35:31 +0600

    Bracnh update

--- All information.py
+++ All information.py
@@ -1,18 +1,22 @@
+import keyword
+
 print(r"input of any functions \n will not work")
 print("This is the line \n breaker")
 
-import keyword
 
-print(keyword.kwlist)    # Shows the keyword libreary
-print(keyword.iskeyword("And"))   # Checks if And is a keyword
+print(keyword.kwlist)  # Shows the keyword libreary
+print(keyword.iskeyword("And"))  # Checks if And is a keyword
 
 
 def add(a, b):
     a = 10
-    
+
     """This is the new line """
     """Return the sum of given arguments."""
     return a + b
 
-print(add(10,200))
+
+print("This text is for the bracnh update and use.")
+
+print(add(10, 200))
 print("THIS IS THE NEW LINE MODIFIED FOR GITHUB.")
